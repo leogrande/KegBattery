d837ab088b6f51a5a77f1fe63c4ee79241101972
Leo Grande <leosgrande1@gmail.com>
2019-06-23 22:37:40 -0400

    Skeleton for full web application that includes template and static folders for html and css files

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Main python file to run KegBattery
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "The KegBattery web application!"
+def init_page(name=None):
+    return render_template('main_page.html', name=name)
 
 
 # Web page will be available at http://localhost:5000/
